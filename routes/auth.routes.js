8216289253f32d5e19912776e8f04a456252c9d7
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-09-01 06:54:16 +0200

    checking if signup hash works

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 const router = new Router();
-
+//Add Bcrypt here
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
 //get routes Here we will display the signup fors for users
 
 router.get('/signup', (req,res) => res.render('auth/signup'));
 //post routes
 router.post('/signup',(req,res,next) =>{
-  console.log('Your form data: ', req.body);
+  const { username, email, password } = req.body;
+
+  bcryptjs
+  .genSalt(saltRounds)
+  .then(salt => bcryptjs.hash(password,salt))
+  .then(hashedPassword => {
+    console.log(`Password hash: ${hashedPassword}`);
+  })
+  .catch(error => next(error));
 });
 
 module.exports = router;

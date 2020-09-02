f728a77ffa7550bc6c10cef7297483bc62d1f408
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-09-02 07:45:43 +0200

    fix login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -51,5 +51,30 @@ if (!regex.test(password)) {
   });
 });
 
+router.get('/login',(req,res) => res.render('auth/login'));
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 module.exports = router;
